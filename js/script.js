7daae438fd0c43334547c05f2682c5e230d4f29c
yupjung0408 <kjy2782@naver.com>
2025-01-16 09:30:15 +0900

    슬라이더 완성

---
+++ js/script.js
@@ -0,0 +1,35 @@
+AOS.init();
+
+/* visual-slider */
+var swiper = new Swiper('.visual-slider', {
+    loop: true, // 반복 */
+    autoplay: {
+        delay: 10000,
+    },
+    speed: 1000,
+
+    pagination: {
+        el: '.visual-slider-pagination',
+        type: 'fraction',
+        clickable: true,
+    },
+    navigation: {
+        nextEl: '.visual-slider-button-next',
+        prevEl: '.visual-slider-button-prev',
+    },
+});
+
+/* news-slider */
+var swiper = new Swiper('.news-slider', {
+    loop: true,
+
+    pagination: {
+        el: '.news-slider-pagination',
+        type: 'fraction',
+        clickable: true,
+    },
+    navigation: {
+        nextEl: '.news-slider-button-next',
+        prevEl: '.news-slider-button-prev',
+    },
+});
