354d5cdb30920487629e39ba3c6d52592f04892f
yupjung0408 <kjy2782@naver.com>
2025-02-12 15:19:25 +0900

    자바스크립트 헤더완성

--- js/script.js
+++ js/script.js
@@ -1,60 +1,126 @@
-AOS.init();
+$(function () {
+    AOS.init();
 
-/* visual-slider */
-var swiper = new Swiper('.visual-slider', {
-    loop: true, // 반복 */
-    // autoplay: {
-    //     delay: 10000,
-    // },
-    // speed: 1000,
+    // GNB
+    // 대상을 변수에 저장
+    const $window = $(window);
+    const $header = $('header');
+    const $subMenu = $('.submenu');
+    const $menu = $('.gnb > li');
+    const duration = 300;
+    console.log($menu);
 
-    pagination: {
-        el: '.visual-slider-pagination',
-        type: 'fraction',
-        clickable: true,
-    },
-    navigation: {
-        nextEl: '.visual-slider-button-next',
-        prevEl: '.visual-slider-button-prev',
-    },
-});
+    let lastScrollTop = 0;
 
-/* news-slider */
-var swiper = new Swiper('.news-slider', {
-    loop: true,
-    // autoplay: {
-    //     delay: 3000,
-    // },
-    // speed: 500,
+    function toggleHeaderVisibility(isVisible) {
+        if (isVisible) {
+            $header.removeClass('hide');
+        } else {
+            $header.addClass('hide');
+        }
+    }
 
-    pagination: {
-        el: '.news-slider-pagination',
-        type: 'fraction',
-        clickable: true,
-    },
-    navigation: {
-        nextEl: '.news-slider-button-next',
-        prevEl: '.news-slider-button-prev',
-    },
+    // 메뉴 영역에 마우스가 들어오면(event: mouseenter)
+    $menu.on('mouseenter', function () {
+        // 그 영역 하위 요소 중에서 submenu 찾아서 슬라이드 다운
+        $(this).find($subMenu).stop().slideDown(duration);
+        // $subMenu.slideDown(duration); // 모든 서브메뉴 떨어뜨리기
 
-    // 모바일의 세팅으로 시작
-    slidesPerView: 1,
-    slidesPerGroup: 1,
-    // spaceBetween: 20,
+        // 메뉴 활성화 표시: on클래스 부여
+        $(this).addClass('on');
+    });
 
-    // 반응형 분기점
-    breakpoints: {
-        // 375픽셀 이상
-        376: {
-            slidesPerView: 2,
-            slidesPerGroup: 2,
-            spaceBetween: 100,
+    // 메뉴 영역에 마우스가 나가면
+    $menu.on('mouseleave', function () {
+        // submenu 찾아서 슬라이드 업
+        $subMenu.stop().slideUp(duration);
+        $menu.removeClass('on');
+    });
+
+    // 마우스 휠을 조작했을 때
+    $window.on('wheel', (e) => {
+        // console.log(e);
+
+        if (e.originalEvent.deltaY < 0) {
+            // 휠을 올렸을 때 header가 보임(hide클래스 삭제)
+            toggleHeaderVisibility(true);
+        } else {
+            toggleHeaderVisibility(false);
+        }
+    });
+
+    // 스크롤 이벤트가 일어나면
+    $window.on('scroll', () => {
+        const scrollTop = $window.scrollTop();
+        console.log(scrollTop);
+
+        // 스크롤한 값이 저장된(마지막에 위치했던) 스크롤 값보다 작다면
+        if (scrollTop < lastScrollTop) {
+            toggleHeaderVisibility(true);
+        } else {
+            toggleHeaderVisibility(false);
+        }
+
+        // 마지막 스크롤 값을 갱신
+        lastScrollTop = scrollTop;
+    });
+
+    /* visual-slider */
+    var swiper = new Swiper('.visual-slider', {
+        loop: true, // 반복 */
+        // autoplay: {
+        //     delay: 10000,
+        // },
+        // speed: 1000,
+
+        pagination: {
+            el: '.visual-slider-pagination',
+            type: 'fraction',
+            clickable: true,
         },
-        // 1025픽셀 이상
-        1440: {
-            slidesPerView: 3,
-            slidesPerGroup: 3,
-            spaceBetween: 80,
+        navigation: {
+            nextEl: '.visual-slider-button-next',
+            prevEl: '.visual-slider-button-prev',
         },
-    },
+    });
+
+    /* news-slider */
+    var swiper = new Swiper('.news-slider', {
+        loop: true,
+        // autoplay: {
+        //     delay: 3000,
+        // },
+        // speed: 500,
+
+        pagination: {
+            el: '.news-slider-pagination',
+            type: 'fraction',
+            clickable: true,
+        },
+        navigation: {
+            nextEl: '.news-slider-button-next',
+            prevEl: '.news-slider-button-prev',
+        },
+
+        // 모바일의 세팅으로 시작
+        slidesPerView: 1,
+        slidesPerGroup: 1,
+        // spaceBetween: 20,
+
+        // 반응형 분기점
+        breakpoints: {
+            // 375픽셀 이상
+            376: {
+                slidesPerView: 2,
+                slidesPerGroup: 2,
+                spaceBetween: 100,
+            },
+            // 1025픽셀 이상
+            1440: {
+                slidesPerView: 3,
+                slidesPerGroup: 3,
+                spaceBetween: 80,
+            },
+        },
+    });
 });
